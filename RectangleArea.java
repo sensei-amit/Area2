d0c7140100a0aad1a14a7c67b21585086c179c7b
Amit <164203799+sensei-amit@users.noreply.github.com>
2025-05-27 17:25:10 +0530

    Update RectangleArea.java

--- RectangleArea.java
+++ RectangleArea.java
@@ -1,9 +1,8 @@
-class RectangleArea {
+class RectanglePerimeter {
     private double length;
     private double breadth;
 
-
-    public RectangleArea(double length, double breadth) {
+    public RectanglePerimeter(double length, double breadth) {
         this.length = length;
         this.breadth = breadth;
     }
@@ -13,19 +12,20 @@ class RectangleArea {
         return length * breadth;
     }
 
-
-    public void displayArea() {
-        System.out.println("Rectangle Area: " + calculateArea());
+    public double calculatePerimeter() {
+        return 2 * (length + breadth);
     }
-    
+
+    public void displayRectangleDetails() {
+        System.out.println("Rectangle Area: " + calculateArea());
+        System.out.println("Rectangle Perimeter: " + calculatePerimeter());
+    }
+
     public static void main(String[] args) {
         CircleArea circle = new CircleArea(5);
         RectanglePerimeter rectangle = new RectanglePerimeter(4, 7);
 
         circle.displayCircleDetails();
         rectangle.displayRectangleDetails();
-    }
+    }
 }
-
-
-
